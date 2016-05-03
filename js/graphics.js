d8148ad0f26b489418a8c2cb6f5976f4e6f677ff
Rafael Negherbon <rafael.negherbon@gmail.com>
2016-05-03 10:02:35 -0300

    endpoints graph

--- js/graphics.js
+++ js/graphics.js
@@ -1,3 +1,4 @@
+'use strict';
 var API = 'http://poker-metrics.localhost:3000';
 var Graphic = {};
 
@@ -5,8 +6,7 @@ Graphic.pointsPerSprint = function pointsPerSprint(target) {
   $.getJSON(API + '/issues', onGetData);
 
   function onGetData(issues) {
-    var sprintsName = Object.keys(issues);
-    var arr = Object.keys(issues).map(function(sprintName) { return [sprintName, issues[sprintName]];  });
+    var arr = objectToArray(issues);
     arr.unshift(['sprint name', 'points']);
     var data = google.visualization.arrayToDataTable(arr);
     var options = {
@@ -23,4 +23,31 @@ Graphic.pointsPerSprint = function pointsPerSprint(target) {
     var chart = new google.visualization.ColumnChart(document.getElementById(target));
     chart.draw(data, options);
   }
+};
+
+Graphic.pointsPerEndpoint = function pointsPerEndpoint(target) {
+  $.getJSON(API + '/endpoints', onGetData);
+
+  function onGetData(endpoints) {
+    var arr = objectToArray(endpoints);
+    arr.unshift(['endpoint name', 'points']);
+    var data = google.visualization.arrayToDataTable(arr);
+    var options = {
+      title: 'Endpoints Points',
+      curveType: 'function',
+      legend: { position: 'bottom' },
+      vAxis: {
+        viewWindow: {
+          min: 0,
+          max: 100
+        }
+      }
+    }
+    var chart = new google.visualization.ColumnChart(document.getElementById(target));
+    chart.draw(data, options);
+  }
+};
+
+function objectToArray(obj) {
+  return Object.keys(obj).map(function(key) { return [key, obj[key]];  });
 }
