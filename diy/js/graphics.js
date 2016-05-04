737f6c8f0f9c8ec33ec6db575386a820db5d0fe3
Rafael Negherbon <rafael.negherbon@gmail.com>
2016-05-04 10:39:46 -0300

    api location

--- diy/js/graphics.js
+++ diy/js/graphics.js
@@ -1,5 +1,5 @@
 'use strict';
-var API = 'http://poker-metrics.localhost:3000';
+var API = 'http://pokerapi-contaazul.rhcloud.com';
 var Graphic = {};
 
 Graphic.pointsPerSprint = function pointsPerSprint(target) {
